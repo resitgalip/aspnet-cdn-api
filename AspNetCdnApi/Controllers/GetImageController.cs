c9614ef49c9fc97f52d1234c41fdfc5aa20dca92
Entegi <mail@entegi.com>
2022-10-22 21:37:27 -0400

    First Initial

---
+++ AspNetCdnApi/Controllers/GetImageController.cs
@@ -0,0 +1,144 @@
+﻿using ImageMagick;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AspNetCdnApi.Controllers
+{
+    public class GetImageController : Controller
+    {
+        [HttpGet("/{*path}"), DisableRequestSizeLimit]
+        public IActionResult GetPhotos(string path)
+        {
+            try
+            {
+
+                var site_info = path.Split("/").Where(x => !string.IsNullOrEmpty(x)).ToArray(); //arrays..
+
+                if (site_info.Length < 2)
+                {
+                    return NoContent();
+                }
+
+                var site_only_path_array = site_info.Skip(1).SkipLast(1).ToArray(); ///data/uploads/a/al/ali
+                var site_url = site_info[0]; 
+                var web_url = Path.Combine("https://", site_url.Replace("_", ".")); 
+                var image = Path.GetFileName(path); //image.jpg
+                var imageLocalPath = Path.Combine(Directory.GetCurrentDirectory(), "images/", path);
+                var imageFolderLocalPath = Path.Combine(Directory.GetCurrentDirectory(), "images/", string.Join('/', site_info.SkipLast(1)));
+                var imageCacheFolderLocalPath = Path.Combine(Directory.GetCurrentDirectory(), "images/", site_url, "cache/", string.Join('/', site_only_path_array));
+                var originalUrl = web_url + "/" + String.Join('/', site_info.Skip(1));
+                var originalUrlNoImage = web_url + "/" + String.Join('/', site_info.Skip(1).SkipLast(1));
+
+                string pattern = @"-\d{1,9}x\d{1,9}\.";
+                string size = "";
+
+                if (Regex.IsMatch(image, pattern))
+                {
+                    var sub_cache_folder_path = Path.Combine("images", site_url, "cache/");
+
+                    //creating cache folders...
+                    if (!Directory.Exists(imageCacheFolderLocalPath))
+                    {
+                        foreach (var item in site_only_path_array)
+                        {
+                            if (!Directory.Exists(sub_cache_folder_path + item))
+                                Directory.CreateDirectory(sub_cache_folder_path + item);
+
+                            sub_cache_folder_path = sub_cache_folder_path + item + "/";
+                        }
+                    }
+
+                    //getting sizes...
+                    foreach (Match match in Regex.Matches(image, pattern))
+                        size = match.ToString();
+                    size = size.Replace("-", "").TrimEnd('.');
+                    int width = int.Parse(size.Split("x")[0].ToString());
+                    int height = int.Parse(size.Split("x")[1].ToString());
+
+
+                    var pathToRead_cache = Path.Combine(imageCacheFolderLocalPath, image);
+                    pathToRead_cache = Path.Combine(pathToRead_cache.Replace(image, ""), Path.GetFileNameWithoutExtension(image) + ".webp");
+
+                    if (!System.IO.File.Exists(pathToRead_cache))
+                    {
+                        string imageName = Regex.Replace(image, pattern, ".");
+                        var pathToReadNew = Path.Combine(imageCacheFolderLocalPath, imageName);
+                        var finalPath = "";
+                        if (!System.IO.File.Exists(pathToReadNew)) //görsel orjinal hali önceden kayıt edilmediyse orjinal url den indiriliyor.
+                        {
+                            if (!System.IO.File.Exists(imageCacheFolderLocalPath + "/" + imageName))
+                            {
+                                var net = new System.Net.WebClient();
+                                string noCacheOriginalUrl = Regex.Replace(originalUrl, pattern, ".").Replace("image/cache", "image");
+                                net.DownloadFile(noCacheOriginalUrl, imageCacheFolderLocalPath + "/" + imageName);
+                                //return NotFound("image not found");
+                            }
+
+                            finalPath = imageCacheFolderLocalPath + "/" + imageName;
+                        }
+                        else
+                            finalPath = pathToReadNew; 
+
+                        using (var image2 = new MagickImage(finalPath))
+                        {
+                            
+                            image2.Resize(width, height);
+                           
+
+                            int width2 = image2.Width;
+                            int height2 = image2.Height;
+
+                            image2.Extent(width, height, Gravity.Center, MagickColors.White);
+
+                            image2.Strip();
+                     
+                            image2.Quality = 90;
+                            image2.SetCompression(CompressionMethod.LosslessJPEG);
+
+                            image2.Write(pathToRead_cache);
+                        }
+
+                    }
+                    var result_image = System.IO.File.OpenRead(pathToRead_cache);
+                    return File(result_image, "image/jpeg");
+
+                }
+                else
+                {
+
+                    //Directory Creating...
+                    if (!Directory.Exists(imageFolderLocalPath))
+                    {
+                        var sub_folder_path = "images/" + site_url + "/";
+                        foreach (var item in site_only_path_array)
+                        {
+                            if (!Directory.Exists(sub_folder_path + item))
+                                Directory.CreateDirectory(sub_folder_path + item);
+
+                            sub_folder_path = sub_folder_path + item + "/";
+                        }
+                    }
+
+                    if (!System.IO.File.Exists(imageLocalPath))
+                    {
+                        var net = new System.Net.WebClient();
+                        net.DownloadFile(originalUrl, imageLocalPath);
+                    }
+                    var img = Image.FromFile(imageLocalPath);
+
+                    var result_image = System.IO.File.OpenRead(imageLocalPath);
+                    return File(result_image, "image/jpeg");
+
+                }
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
---
+++ AspNetCdnApi/Controllers/UploadImageController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+
+namespace AspNetCdnApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadImageController : Controller
+    {
+       
+
+        [HttpPost]
+        public IActionResult UploadFile()
+        {
+            try
+            {
+                if (Request.ContentLength == 0)
+                    return NoContent();
+
+                var file = Request.Form.Files[0];
+                var site = Request.Form["site"].ToString();
+                var user_id = Request.Form["user_id"].ToString();
+
+
+                var folderName = Path.Combine("images", site);
+                var pathWebSite = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                var pathToSave = Path.Combine(pathWebSite, user_id);
+
+                if (!Directory.Exists(pathWebSite))
+                {
+                    Directory.CreateDirectory(pathWebSite);
+                }
+
+                if (file.Length > 0 && !string.IsNullOrEmpty(site) && !string.IsNullOrEmpty(user_id))
+                {
+
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + fileName;
+
+                    var fullPath = Path.Combine(pathToSave, uniqueFileName);
+
+                    if (!Directory.Exists(pathToSave))
+                        Directory.CreateDirectory(pathToSave);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    var siteUrl = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("SiteUrl").Value;
+                    var imagePath = siteUrl + site + "/" + user_id + "/" + uniqueFileName;
+
+
+                    string result = "";
+                    try
+                    {
+                        System.Drawing.Image imgInput = System.Drawing.Image.FromFile(fullPath);
+                        System.Drawing.Graphics gInput = System.Drawing.Graphics.FromImage(imgInput);
+                        System.Drawing.Imaging.ImageFormat thisFormat = imgInput.RawFormat;
+
+                        return Ok(new { imagePath });
+
+                    }
+                    catch (Exception ex)
+                    {
+                        return BadRequest();
+                    }
+                    return BadRequest();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+    }
+}
